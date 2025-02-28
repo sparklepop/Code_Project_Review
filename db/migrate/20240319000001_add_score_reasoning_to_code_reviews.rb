fcf2ac41c03e5b5c13c96a3bd1165f1562ed90b0
kathrynohara <20523663+sparklepop@users.noreply.github.com>
2025-02-28 16:59:59 -0700

    Remove unused code review migration files

--- db/migrate/20240319000001_add_score_reasoning_to_code_reviews.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddScoreReasoningToCodeReviews < ActiveRecord::Migration[7.1]
-  def change
-    # No need for a new column since we're storing in the existing JSON columns
-    # Just update the model to handle the new attributes
-  end
-end 
